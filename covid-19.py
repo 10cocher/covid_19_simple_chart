8db7d0292241f3d1f5629a798b5732262081498c
Emmanuel Cocher <ecocher@idmog.com>
2020-03-27 19:32:55 +0100

    modify main .py script file to accomodate the new version of the libcovid module

--- covid-19.py
+++ covid-19.py
@@ -1,4 +1,10 @@
+import os
 import libcovid19 as covid
+
+# folder where to save output plots
+out_dir = os.path.join(os.getcwd(), 'plots')
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
 
 # get the latest cases and death information
 df = covid.get_latest_data()
@@ -14,5 +20,5 @@ df = covid.process_data_for_plot(
 chart = covid.get_altair_chart(df)
 
 # save the chart as a html file
-path_output_html_file = os.path.join('plots','charts_covid_19.html')
+path_output_html_file = os.path.join(out_dir, 'charts_covid_19.html')
 chart.save(path_output_html_file)
